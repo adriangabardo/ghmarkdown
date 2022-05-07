967ae44e10b7903b6b64eb866e12d5fda1e5536f
Adrian Gabardo <adrian.gabardo@argenti.com.au>
2022-05-07 21:58:25 +1000

    refactor: Header using ColorSchemeToggle

--- components/Navigation/Header.tsx
+++ components/Navigation/Header.tsx
@@ -1,26 +1,13 @@
-import { ActionIcon, ColorScheme, Group, Header as MantineHeader } from '@mantine/core';
-import { setCookies } from 'cookies-next';
-import { AppProps } from 'next/app';
-import { useState } from 'react';
-import { MoonStars, Sun } from 'tabler-icons-react';
+import { Group, Header as MantineHeader } from '@mantine/core';
+import { ColorSchemeToggle } from '../ColorSchemeToggle/ColorSchemeToggle';
 import DynamicBreadcrumbs from './DynamicBreadcrumbs';
 
-export default function Header(props: AppProps & { colorScheme: ColorScheme }) {
-  const [colorScheme, setColorScheme] = useState<ColorScheme>(props.colorScheme);
-
-  const toggleColorScheme = (value?: ColorScheme) => {
-    const nextColorScheme = value || (colorScheme === 'dark' ? 'light' : 'dark');
-    setColorScheme(nextColorScheme);
-    setCookies('mantine-color-scheme', nextColorScheme, { maxAge: 60 * 60 * 24 * 30 });
-  };
-
+export default function Header() {
   return (
     <MantineHeader height={70} p="md">
       <div style={{ display: 'flex', alignItems: 'center', height: '100%' }}>
         <Group>
-          <ActionIcon variant="default" onClick={() => toggleColorScheme()} size={30}>
-            {colorScheme === 'dark' ? <Sun size={16} /> : <MoonStars size={16} />}
-          </ActionIcon>
+          <ColorSchemeToggle />
           <DynamicBreadcrumbs />
         </Group>
       </div>
