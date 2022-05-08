6a16f56aeea73513956c5d52fcb22f92acd83e48
Adrian Gabardo <adrian.gabardo@argenti.com.au>
2022-05-08 21:14:50 +1000

    support user dark mode preferences

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -50,5 +50,9 @@ export default function App(props: AppProps & { colorScheme: ColorScheme }) {
 }
 
 App.getInitialProps = ({ ctx }: { ctx: GetServerSidePropsContext }) => ({
-  colorScheme: getCookie('mantine-color-scheme', ctx) || 'light',
+  colorScheme:
+    getCookie('mantine-color-scheme', ctx) ??
+    (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches)
+      ? 'dark'
+      : 'light',
 });
